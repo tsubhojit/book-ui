71ee2606fb5bd00979ba651c9ce5ca116d109806
Saurabh Ghoshal <saurabh.ghosal@tcs.com>
2020-06-23 21:03:36 +0530

    SaurabhG-Checking in Changes

--- server/server.js
+++ server/server.js
@@ -46,6 +46,7 @@ server.listen(port, function() {
 
   logger.info(`Server listening on http://localhost:${port}`);
   console.log(`Server listening on http://localhost:${port}`);
+  console.log(`Server Started`);
 
 });
 
