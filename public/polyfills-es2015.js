f0fa347d62c551bcf37bbb14633cb4c141799793
Souvik Sarkar <1622721@TCS.com>
2020-07-06 17:27:13 +0530

    Updating application properties- souvik

--- public/polyfills-es2015.js
+++ public/polyfills-es2015.js
@@ -6466,8 +6466,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\1622721\Downloads\rhos-reservation-app-ui\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\1622721\Downloads\rhos-reservation-app-ui\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! D:\book-ui\client\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! D:\book-ui\client\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
